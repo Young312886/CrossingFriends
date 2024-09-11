033615209365fa3c2dd6a12d333b20098b06d3f1
ju214425 <ju214425@naver.com>
2024-09-12 02:13:15 +0900

    Feat: 캐릭터 이동 기능 구현
    캐릭터 상화좌우 이동 기능 구현
    맵 밖으로 벗어나지 않도록 제한 기능 구현

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    [SerializeField]
+    private int unitMove = 1;
+    [SerializeField]
+    private int minX = -5;
+    [SerializeField]
+    private int maxX = 5;
+    [SerializeField]
+    private int minY = -10;
+    [SerializeField]
+    private int maxY = 10;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+
+    private void Move() {
+        Vector3 moveTo = Vector3.zero;
+        if ( Input.GetKeyDown(KeyCode.UpArrow) ) {
+            moveTo = new Vector3(0, unitMove, 0);
+        } else if ( Input.GetKeyDown(KeyCode.DownArrow) ) {
+            moveTo = new Vector3(0, -unitMove, 0);
+        } else if ( Input.GetKeyDown(KeyCode.LeftArrow) ) {
+            moveTo = new Vector3(-unitMove, 0, 0);
+        } else if ( Input.GetKeyDown(KeyCode.RightArrow) ) {
+            moveTo = new Vector3(unitMove, 0, 0);
+        } 
+
+        Vector3 nextPosition = transform.position + moveTo;
+
+        // 다음 이동할 구역이 지정 구역을 벗어난 경우 
+        nextPosition.x = Mathf.Clamp(nextPosition.x, minX, maxX);
+        nextPosition.y = Mathf.Clamp(nextPosition.y, minY, maxY);
+        transform.position = nextPosition;
+
+        // if ( minX <= nextPosition.x && nextPosition.x <= maxX && minY <= nextPosition.y && nextPosition.y <= maxY ) {
+        //     transform.position = nextPosition;
+        // }
+    }
+}
