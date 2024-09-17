ff4382beee3477aa3b6e65c79156944c7872bf84
fkeldh2089 <fkeldh2089@gmail.com>
2024-09-17 22:53:19 +0900

    Feat : Create Score Panel and Coin Panel just Create Score Panel and Coin Panel, If click CoinPanel(Top Right Side) you can get score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    private int coin = 0;
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void IncreaseCoin()
     {
-        
+        coin += 1;
+        scoreText.SetText(coin.ToString());
     }
 }
